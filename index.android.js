39335d1a751802d6836d5912f08dabb435f7119f
Maurilio Atila <cabelotaina@gmail.com>
2016-12-01 18:16:29 +0100

    initial conf for push notification

--- index.android.js
+++ index.android.js
@@ -13,6 +13,10 @@ import {
   BackAndroid,
 } from 'react-native';
 
+
+import FCM from 'react-native-fcm';
+
+
 import SplashPage from './views/SplashPage';
 import LoginPage from './views/LoginPage';
 import MainPage from './views/MainPage';
@@ -24,6 +28,62 @@ import ErrorPage from './views/ErrorPage';
 var _navigator;
 
 class App extends Component {
+
+  componentDidMount() {
+    FCM.getFCMToken().then(token => {
+      console.log("Token FCM: "+token)
+      // store fcm token in your server
+    });
+    
+    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+      console.log('notification: '+notif);
+    });
+    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+      console.log('refresh subscribe: '+token)
+      // fcm token may not be available on first load, catch it here
+    });
+
+    //FCM.subscribeToTopic('/topics/foo-bar');
+    //FCM.unsubscribeFromTopic('/topics/foo-bar');
+    FCM.getInitialNotification().then(notif=>console.log(notif));
+    FCM.presentLocalNotification({
+        id: "UNIQ_ID_STRING",                               // (optional for instant notification)
+        title: "My Notification Title",                     // as FCM payload
+        body: "My Notification Message",                    // as FCM payload (required)
+        sound: "default",                                   // as FCM payload
+        priority: "high",                                   // as FCM payload
+        click_action: "ACTION",                             // as FCM payload
+        badge: 10,                                          // as FCM payload IOS only, set 0 to clear badges
+        number: 10,                                         // Android only
+        ticker: "My Notification Ticker",                   // Android only
+        auto_cancel: true,                                  // Android only (default true)
+        large_icon: "ic_launcher",                           // Android only
+        icon: "ic_notification",                            // as FCM payload
+        big_text: "Show when notification is expanded",     // Android only
+        sub_text: "This is a subText",                      // Android only
+        color: "red",                                       // Android only
+        vibrate: 300,                                       // Android only default: 300, no vibration if you pass null
+        tag: 'some_tag',                                    // Android only
+        group: "group",                                     // Android only
+        my_custom_data:'my_custom_field_value',             // extra data you want to throw
+        lights: true,                                       // Android only, LED blinking (default false)
+    });
+    FCM.scheduleLocalNotification({
+        fire_date: new Date().getTime(),      //RN's converter is used, accept epoch time and whatever that converter supports
+        id: "UNIQ_ID_STRING",    //REQUIRED! this is what you use to lookup and delete notification. In android notification with same ID will override each other
+        body: "from future past",
+        repeat_interval: "week" //day, hour
+    })
+  }
+  componentWillUnmount() {
+    // prevent leaking
+    this.refreshUnsubscribe();
+    this.notificationUnsubscribe();
+  }
+
+
+
   render() {
     return (
       <Navigator
